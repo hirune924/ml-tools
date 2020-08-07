e44c4156223053b0fce485c370bb635d7aee2a33
hirune924 <nori.o920924@outlook.jp>
2020-08-07 09:25:13 +0900

    restart

--- experiments/baseline.py
+++ experiments/baseline.py
@@ -35,6 +35,19 @@ def main(cfg: DictConfig) -> None:
         ).reset_index(drop=True).to_feather(utils.to_absolute_path(feature_name))
     print(pd.read_feather(utils.to_absolute_path(feature_name)).head())
 
+    feature_name = "features/feature_arithmetic_combi2.ftr"
+    if not os.path.exists(utils.to_absolute_path(feature_name)):
+        print("2-order Arithmetic combinations.")
+        xfeat.Pipeline([
+            xfeat.SelectNumerical(),
+            xfeat.ArithmeticCombinations(
+                exclude_cols=["target"], drop_origin=True, operator="+", r=2,
+            ),
+        ]).fit_transform(
+            pd.read_feather(utils.to_absolute_path("features/train_test.ftr"))
+        ).reset_index(drop=True).to_feather(utils.to_absolute_path(feature_name))
+    print(pd.read_feather(utils.to_absolute_path(feature_name)).head())
+
     feature_name = "features/feature_1way_label_encoding.ftr"
     if not os.path.exists(utils.to_absolute_path(feature_name)):
         print("Categorical encoding using label encoding")
@@ -69,6 +82,25 @@ def main(cfg: DictConfig) -> None:
         ).reset_index(drop=True).to_feather(utils.to_absolute_path(feature_name))
     print(pd.read_feather(utils.to_absolute_path(feature_name)).head())
 
+    ## Load & Set Features
+    print("Load numerical features")
+    df_num = pd.concat(
+        [
+            pd.read_feather(pd.read_feather(utils.to_absolute_path("features/feature_num_features.ftr"))),
+            pd.read_feather(pd.read_feather(utils.to_absolute_path("features/feature_arithmetic_combi2.ftr")))
+        ], axis=1)
+
+    print("Load categorical features")
+    df_cat = pd.concat(
+        [
+            pd.read_feather(pd.read_feather(utils.to_absolute_path("features/feature_1way_label_encoding.ftr"))),
+            pd.read_feather(pd.read_feather(utils.to_absolute_path("features/feature_2way_label_encoding.ftr"))),
+            pd.read_feather(pd.read_feather(utils.to_absolute_path("features/feature_3way__including_Sex_label_encoding.ftr"))),
+        ], axis=1)
+
+    df = pd.concat([df_cat, df_num], axis=1)
+    y_train = df_num["Survived"].dropna()
+    df.drop(["Survived"], axis=1, inplace=True)
 
     ## Training
 
